565df6984c0f7a3bc3f15b8524cb21b2748acddf
7stevecarrasco <33759306+7stevecarrasco@users.noreply.github.com>
2021-11-06 01:23:05 -0400

    Add files via upload

---
+++ wwwroot/js/SubmitOrder.js
@@ -0,0 +1,32 @@
+﻿function ConfirmOrder() {
+    document.location = 'Cart/OrderConfirmation/';
+}
+
+function loadEventInfo(id) {
+    $.get("/Cart/LoadEventInfo/"+ id,
+        function (data) {
+            $('#EventSectionContainer').html(data);
+        });
+}
+
+function selectedEvent(event) {
+    var id = event.value;
+    $.get("/Cart/LoadEventInfo/" + id,
+        function (data) {
+            $('#EventSectionContainer').html(data);
+        });
+}
+
+function OrderConfirmation() {
+    var id = document.getElementById("listbox").value;
+    //$.post("/Cart/OrderConfirmation/" + id,
+    //    function (data) {
+    //    });
+    document.location = 'Cart/OrderConfirmation/' + id;
+    $("#ConfirmOrderModal").modal("hide");
+}
+//$('#ConfirmOrderModal').on('load', function () {
+//    var e = document.getElementById("listbox");
+//    var result = e.options[e.selectedIndex].value;
+//    selectedEvent(result);
+//});
